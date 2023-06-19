b1ffc9352ab7a24de4a6c27105aa002401a97f09
A Stone <astone@heckgrammar.co.uk>
2023-06-19 11:10:12 +0100

    Add project files.

---
+++ 3.2.7-InputOutput-VB/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        'Be able to obtain user input from the keyboard
+        Dim input As String
+        Console.Write("Enter something: ")
+        input = Console.ReadLine ' always assigned as a string but VB will allow you to input into a number/boolean as well
+
+        'Be able to output data and information from a program to the computer display
+        Console.WriteLine(input)
+    End Sub
+
+End Module
